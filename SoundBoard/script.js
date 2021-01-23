b412a62ffc58ef603dabed7066dc25540dcd970b
ProRoger <roger.e.cerpa@gmail.com>
2021-01-23 12:12:00 -0500

    project Dad jokes initial code, styling

---
+++ SoundBoard/script.js
@@ -0,0 +1,24 @@
+const sounds = ['arcade', 'alarm', 'dog', 'fastSmall', 'retro'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('button').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
